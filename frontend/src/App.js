eabe4f1d0b65d5086353ca3b1ab40e8cac0daaeb
Jéssica Cruz <jessica.santos648@gmail.com>
2020-04-09 10:56:13 -0300

    Estilo da tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import Routes from './routes';
 
 import './global.css';
-import Logon from './pages/Logon';
 
 function App() {
   return (
     <div>
-      <Logon />
+      <Routes />
     </div>
   );
 }
